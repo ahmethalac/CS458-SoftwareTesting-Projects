edb0269859ac8c193038ec60b16fe936c6bea515
ahmethalac <ahmetfeyzihalac@gmail.com>
2022-04-22 00:47:21 +0300

    use jest along with selenium

---
+++ Project3/Geolocation.test.js
@@ -0,0 +1,23 @@
+var { Builder, By, Key } = require('selenium-webdriver');
+const { Options } = require('selenium-webdriver/chrome');
+
+describe('Geolocation Service Tests', () => {
+  let driver;
+  const loginPage = 'https://localhost:3000';
+
+  beforeAll(() => {
+    const chromeOptions = new Options();
+    chromeOptions.excludeSwitches('enable-logging');
+
+    driver = new Builder().forBrowser('chrome').setChromeOptions(chromeOptions).build();
+  });
+
+  beforeEach(async () => {
+    await driver.get(loginPage);
+  });
+
+  afterAll(async () => {
+    await driver.quit();
+  });
+  
+});
