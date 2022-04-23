5321a527ff5424aa3d8b13f0bf0edadf93395bfa
ahmethalac <ahmetfeyzihalac@gmail.com>
2022-04-23 15:09:09 +0300

    TDD :: Test Case #5 :: Green Line

--- Project3/script.js
+++ Project3/script.js
@@ -15,12 +15,48 @@ const setCountry = async coordinates => {
     window.infowindow.open(map, marker);
 }
 
-document.getElementById('showCountry').addEventListener('click', async () => {
-    const lat = parseFloat(document.getElementById('latInput').value);
-    const lng = parseFloat(document.getElementById('lngInput').value);
-    if (!lat || !lng) return;
+let timeout;
+const showError = text => {
+    if (timeout) clearTimeout(timeout);
+    document.getElementById('errorText').textContent = text;
+    timeout = setTimeout(() => {
+        document.getElementById('errorText').textContent = '';  
+    }, 2000);
+}
 
-    await setCountry({ lat, lng });
+document.getElementById('showCountry').addEventListener('click', async () => {
+    const lat = document.getElementById('latInput').value;
+    const lng = document.getElementById('lngInput').value;
+
+    if (lat === '') {
+        showError('Latitude cannot be empty!');
+        return;
+    }
+    if (lng === '') {
+        showError('Longitude cannot be empty!');
+        return;
+    }
+
+    const numberLat = parseFloat(lat);
+    const numberLng = parseFloat(lng);
+    
+    if (isNaN(numberLat)) {
+        showError('Latitude must be number!');
+        return;
+    }
+    if (isNaN(numberLng)) {
+        showError('Longitude must be number!');
+        return;
+    }
+    if (numberLat <= -90 || numberLat >= 90) {
+        showError('Latitude must be in range (-90, 90)!');
+        return;
+    }
+    if (numberLng <= -180 || numberLng >= 180) {
+        showError('Longitude must be in range (-180, 180)!');
+        return;
+    }
+    await setCountry({ lat: numberLat, lng: numberLng });
 });
 
 document.getElementById('showNorthPoleDistance-autoGPS').addEventListener('click', async () => {
