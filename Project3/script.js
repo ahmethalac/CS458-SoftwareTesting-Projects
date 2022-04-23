9d58742376a4f2996a58122e7cee522aa734a907
ahmethalac <ahmetfeyzihalac@gmail.com>
2022-04-23 15:09:25 +0300

    TDD :: Test Case #5 :: Refactor

--- Project3/script.js
+++ Project3/script.js
@@ -1,3 +1,4 @@
+let marker;
 const setCountry = async coordinates => {
     const { results } = await window.geocoder.geocode({ location: coordinates });
     const { formatted_address: country } = results.find(result => result?.types.includes('country')) || {};
@@ -6,12 +7,30 @@ const setCountry = async coordinates => {
     document.getElementById('country').textContent = country;
 
     window.map.setZoom(6);
-    const marker = new google.maps.Marker({
+
+    if (marker) marker.setMap(null);
+    marker = new google.maps.Marker({
         position: coordinates,
         map: window.map
     });
     window.map.setCenter(coordinates);
     window.infowindow.setContent(country);
+    window.infowindow.open(map, marker);
+}
+
+const setLocation = async coordinates => {
+    const distanceToNorthPole = google.maps.geometry.spherical.computeDistanceBetween({ lat: 90, lng: 0 }, coordinates);
+    const distanceInKms = Math.floor(distanceToNorthPole / 1000);
+    document.getElementById('northPole').textContent = distanceInKms;
+
+    window.map.setZoom(6);
+    if (marker) marker.setMap(null);
+    marker = new google.maps.Marker({
+        position: coordinates,
+        map: window.map
+    });
+    window.map.setCenter(coordinates);
+    window.infowindow.setContent(`${distanceInKms} kms`);
     window.infowindow.open(map, marker);
 }
 
@@ -24,17 +43,14 @@ const showError = text => {
     }, 2000);
 }
 
-document.getElementById('showCountry').addEventListener('click', async () => {
-    const lat = document.getElementById('latInput').value;
-    const lng = document.getElementById('lngInput').value;
-
+const validateLatLng = (lat, lng) => {
     if (lat === '') {
         showError('Latitude cannot be empty!');
-        return;
+        return false;
     }
     if (lng === '') {
         showError('Longitude cannot be empty!');
-        return;
+        return false;
     }
 
     const numberLat = parseFloat(lat);
@@ -42,41 +58,50 @@ document.getElementById('showCountry').addEventListener('click', async () => {
     
     if (isNaN(numberLat)) {
         showError('Latitude must be number!');
-        return;
+        return false;
     }
     if (isNaN(numberLng)) {
         showError('Longitude must be number!');
-        return;
+        return false;
     }
     if (numberLat <= -90 || numberLat >= 90) {
         showError('Latitude must be in range (-90, 90)!');
-        return;
+        return false;
     }
     if (numberLng <= -180 || numberLng >= 180) {
         showError('Longitude must be in range (-180, 180)!');
-        return;
+        return false;
     }
-    await setCountry({ lat: numberLat, lng: numberLng });
-});
+    return { lat: numberLat, lng: numberLng };
+};
 
-document.getElementById('showNorthPoleDistance-autoGPS').addEventListener('click', async () => {
-    window.navigator.geolocation.getCurrentPosition(({ coords: { latitude: lat, longitude: lng }}) => {
-        const distanceToNorthPole = google.maps.geometry.spherical.computeDistanceBetween({ lat: 90, lng: 0 }, { lat, lng });
-        document.getElementById('northPole').textContent = Math.floor(distanceToNorthPole / 1000);
-    });
-});
+document.getElementById('showCountry').addEventListener('click', async () => {
+    const lat = document.getElementById('latInput').value;
+    const lng = document.getElementById('lngInput').value;
 
-document.getElementById('showNorthPoleDistance').addEventListener('click', async () => {
-    const lat = parseFloat(document.getElementById('latInput').value);
-    const lng = parseFloat(document.getElementById('lngInput').value);
-    if (!lat || !lng) return;
-
-    const distanceToNorthPole = google.maps.geometry.spherical.computeDistanceBetween({ lat: 90, lng: 0 }, { lat, lng });
-    document.getElementById('northPole').textContent = Math.floor(distanceToNorthPole / 1000);
+    const validatedCoordinates = validateLatLng(lat, lng);
+    if (!validatedCoordinates) return;
+    await setCountry(validatedCoordinates);
 });
 
 document.getElementById('showCountry-autoGPS').addEventListener('click', async () => {
     window.navigator.geolocation.getCurrentPosition(async ({ coords: { latitude: lat, longitude: lng }}) => {
         await setCountry({ lat, lng });
+    });
+});
+
+document.getElementById('showNorthPoleDistance').addEventListener('click', async () => {
+    const lat = document.getElementById('latInput').value;
+    const lng = document.getElementById('lngInput').value;
+    
+    const validatedCoordinates = validateLatLng(lat, lng);
+    if (!validatedCoordinates) return;
+    
+    setLocation(validatedCoordinates);
+});
+
+document.getElementById('showNorthPoleDistance-autoGPS').addEventListener('click', async () => {
+    window.navigator.geolocation.getCurrentPosition(async ({ coords: { latitude: lat, longitude: lng }}) => {
+        setLocation({ lat, lng });
     });
 });
